65f9198c2040347f6c80d2e8d375d6059609efd7
Khanh Bui <pkkhanhbui@gmail.com>
2023-04-26 10:21:49 -0400

    update build for gh-pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import Nav from "./components/Navigation/Navigation";
 import Home from "./components/Home/Home";
 import About from "./components/About/About";
